f27db4caf3267b03968177d666854e9a8af26f4b
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-07-16 21:25:17 +0500

    working on post report api

--- database/routes/reportRoute.js
+++ database/routes/reportRoute.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const { addReport } = require("../controllers/reportController");
 const router = express.Router();
-
-
-
 router.post("/addreport", addReport);
-
-
 module.exports = router;
